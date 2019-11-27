68494dde30121fe77caf0b8d1e81cef5d4872581
Sudipta Mondal <sudiptamondal05@gmail.com>
2019-11-27 14:49:51 +0530

    added site_up feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :app_up
+  
+  private
+  
+    def app_up
+      begin
+        site_up  = Property.where(key: "SITE_UP").first.value == "UP" ? true : false
+      rescue Exception => e
+        site_up = false  
+      end
+        if site_up?
+          redirect_to :root
+        else
+          redirect_to "/down.html"
+        end
+    end
+
 end
